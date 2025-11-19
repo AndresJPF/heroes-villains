3a8ed3d009eb902c669c47a72498618bf8ac8161
AndresJPF <aandrespalacio29@gmail.com>
2025-11-19 16:43:23 -0500

    Adaptando a Mobil

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'heroes-villains',
+  appId: 'com.andres.heroesvillains',
+  appName: 'Heroes Villains',
   webDir: 'www'
 };
 
